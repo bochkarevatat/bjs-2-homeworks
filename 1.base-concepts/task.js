095c623016fb29e786228301f4c7e00d4ecb5575
bochkarevatat <tatianaistorik@gmail.com>
2021-12-11 19:44:47 +0400

    I finished exercise one

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,52 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
+  const D = b ** 2 - (4 * a * c);
+  let x1 = 0;
+  let x2 = 0;
+
+  if (D == 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    arr[0] = x1;
+  }
+  if (D > 0) {
+    console.log(D)
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+
+    arr[0] = x1;
+    arr[1] = x2;
+
+  } else if (D < 0) {
+
+    return [];
+  }
+
   // код для задачи №1 писать здесь
   return arr; // array
-}
+};
+
+
+solveEquation();
+console.log(solveEquation(1, 5, 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
