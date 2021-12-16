3963ac44c19381e66d805f914464f009f71a8187
bochkarevatat <tatianaistorik@gmail.com>
2021-12-16 22:51:43 +0400

    Finished task #2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,21 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  function everyCompare(index){
-    
-    return arr1[index] === arr2[index]; 
-    
+  function everyCompare(index) {
+
+    return arr1[index] === arr2[index];
+
   }
-result = arr1.length === arr2.length && arr1.every(everyCompare)
+  result = arr1.length === arr2.length && arr1.every(everyCompare)
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr
+  .filter((arr) => arr > 0)
+  .filter((arr) => arr % 3 === 0)
+  .map((arr) => arr * 10);
 
   // Ваш код
 
