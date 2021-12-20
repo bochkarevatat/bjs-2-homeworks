7f00b583ea58ef4304f6c6999883f4f34f123c63
bochkarevatat <tatianaistorik@gmail.com>
2021-12-20 13:28:16 +0400

    Finished task one, but many questions left

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
 
-}
+    this.name = name,
+    this.gender = gender,
+    this.age = age
+
+};
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...theMark){
+  if (this.marks === undefined) {
+    this.marks = [];
+    theMark.map(item => this.marks.push(item));
+  } else {
+    theMark.map(item => this.marks.push(item));
+  }
+
+};
+
+Student.prototype.getAverage = function (){
+  return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason){
+delete this.subject;
+delete this.marks;
+this.excluded = reason;
+
+};
