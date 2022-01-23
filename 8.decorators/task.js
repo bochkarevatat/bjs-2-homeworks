427f4d344008db2cda81006baebda3f7c7a7cbc6
bochkarevatat <tatianaistorik@gmail.com>
2022-01-24 00:09:35 +0400

    Finished task#1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,32 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
-}
+
+  let cache = [];
+
+  function wrapper(...args) {
+
+    const hash = args.toString(); 
+    let idx = cache.findIndex((item) => item.hash === hash);
+    console.log(idx);
+
+    if (idx !== -1) {
+      console.log("Из кэша: " + cache[hash]);
+      return "Из кэша: " + cache[hash];
+    } else {
+      let result = func(...args);
+      cache[hash] = result
+      cache.push({hash});
+      if (cache.length > 5) {
+        cache.shift();
+
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+
+
+  }
+  return wrapper;
+};
 
 
 function debounceDecoratorNew(func) {
